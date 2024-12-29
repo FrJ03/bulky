7970afbff5ff3069dab0373f0f4d96680aa1a1cb
FrJ03 <franjomor@gmail.com>
2024-12-29 14:03:58 +0100

    feat: create category

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using bulky.Data;
+using bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bulky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;  
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
